138d80c140013f051d888e5951b62cf979c4d7f4
caojian <caojian@hualala.com>
2019-01-17 10:32:32 +0800

    git提交

--- src/main/java/com/chaos/demo/demo1106/ArrayQueue.java
+++ src/main/java/com/chaos/demo/demo1106/ArrayQueue.java
@@ -29,7 +29,7 @@ public class ArrayQueue {
             //队列已满
             return false;
         }
-        items[tail]=item;
+        items[tail]=item; 
         tail++;
         return true;
     }
