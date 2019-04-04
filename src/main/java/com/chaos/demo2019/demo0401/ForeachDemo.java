4e8cc591486ff3975933e50ebd5f4d73b6ac6e5f
liaopeng <18610030642@163.com>
2019-04-04 16:16:14 +0800

    treemap

--- src/main/java/com/chaos/demo2019/demo0401/ForeachDemo.java
+++ src/main/java/com/chaos/demo2019/demo0401/ForeachDemo.java
@@ -11,31 +11,31 @@ import java.util.function.BiConsumer;
  **/
 public class ForeachDemo {
     public static void main(String[] args) {
-//       Map<String,Object> treeMap = new TreeMap<>(new Comparator<String>() {
-//           @Override
-//           public int compare(String o1, String o2) {
-//               return -1*o1.compareTo(o2);
-//           }
-//       });
-//
-//       treeMap.put("2","213");
-//       treeMap.put("1",321);
-//       treeMap.put("3",3453);
-//       treeMap.forEach(new BiConsumer<String, Object>() {
-//           @Override
-//           public void accept(String s, Object o) {
-//               System.out.println(s);
-//           }
-//       });
-
-       List<String> list = new ArrayList<String>(){
-           {
-               this.add("123");
+       Map<String,Object> treeMap = new TreeMap<>(new Comparator<String>() {
+           @Override
+           public int compare(String o1, String o2) {
+               return -1*o1.compareTo(o2);
            }
-       };
-       list.forEach(s -> {
-           System.out.println(s);
        });
+
+       treeMap.put("2","213");
+       treeMap.put("1",321);
+       treeMap.put("3",3453);
+       treeMap.forEach(new BiConsumer<String, Object>() {
+           @Override
+           public void accept(String s, Object o) {
+               System.out.println(s);
+           }
+       });
+
+//       List<String> list = new ArrayList<String>(){
+//           {
+//               this.add("123");
+//           }
+//       };
+//       list.forEach(s -> {
+//           System.out.println(s);
+//       });
     }
 
 }
